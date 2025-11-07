9acf666edbf027ac7fc3ee28235a792a37a62289
mih <keniamsoaress@gmail.com>
2025-11-07 12:59:16 -0300

    fix: update API key in production environment and use environment variable for base URL in axios client

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiKey = import.meta.env.VITE_API_KEY_SECRET;
 
 const apiClient = axios.create({
-  baseURL: 'https://cine-safico.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://cine-safico.onrender.com',
   timeout: 45000,
   headers: {
     // 'x-api-key': apiKey,
